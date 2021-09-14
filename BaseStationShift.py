680c04d229867bb3771b22e2bd62f72b2cb01afd
Branyon Apel <Branyon.Apel@Swarmfarm.com>
2021-09-14 15:41:52 +1000

    WIP

--- BaseStationShift.py
+++ BaseStationShift.py
@@ -1,6 +1,10 @@
 import csv, sys
 reader = csv.reader(open(sys.argv[1]), delimiter="\t")
+writer = csv.writer(open(sys.argv[2], 'w'), delimiter="\t")
 for row in reader:
-    row[0]=str(float(row[0])+0.1)
-    row[1]=str(float(row[1])+0.1)
+    row[0]=str(float(row[0])+float(sys.argv[3]))
+    row[1]=str(float(row[1])+float(sys.argv[4]))
+
     print(row[0] + ',' + row[1])
+
+    writer.writerow(row)
