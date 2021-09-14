56e705b2562904950679eb604ca41d21292a6ee7
Branyon Apel <Branyon.Apel@Swarmfarm.com>
2021-09-14 17:25:27 +1000

    WIP

--- BaseStationShifter_Definition.py
+++ BaseStationShifter_Definition.py
@@ -19,7 +19,30 @@ def load_json(string):
 
 json_file = load_json(sys.argv[1])
 
-print(json_file)
+for element in json_file["paddock"]:
+    print(element)
+
+#print()
+#print(json_file["paddock"]["buffer"])
+#print()
+#print(json_file["paddock"]["type"])
+#print()
+#print(json_file["paddock"]["ab"])
+#print()
+#print(json_file["paddock"]["obstacles"])
+#print()
+#print(json_file["paddock"]["green_area"])
+#print()
+#print(json_file["paddock"]["boundaries"])
+
+for elem in json_file["paddock"]["obstacles"]:
+    for point in elem["boundaries"]:
+        point["lat"] = point["lat"]+float(sys.argv[3])
+        print(point["lat"])
+        print(point["lon"])
+        print()
+    print()
+
 
 with open(sys.argv[2], "w") as write_file:
     json.dump(json_file, write_file)
