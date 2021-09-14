e357fdb761cce9b3fda12e1a309cf9ca13673285
Branyon Apel <Branyon.Apel@Swarmfarm.com>
2021-09-15 06:49:31 +1000

    WIP

--- BaseStationShifter_Definition.py
+++ BaseStationShifter_Definition.py
@@ -19,30 +19,50 @@ def load_json(string):
 
 json_file = load_json(sys.argv[1])
 
-for element in json_file["paddock"]:
-    print(element)
+#buffer
+#type
+#ab
+#obstacles
+#green_area
+#boundaries
 
-#print()
-#print(json_file["paddock"]["buffer"])
-#print()
-#print(json_file["paddock"]["type"])
-#print()
-#print(json_file["paddock"]["ab"])
-#print()
-#print(json_file["paddock"]["obstacles"])
-#print()
-#print(json_file["paddock"]["green_area"])
-#print()
-#print(json_file["paddock"]["boundaries"])
+#print(json_file)
 
 for elem in json_file["paddock"]["obstacles"]:
     for point in elem["boundaries"]:
         point["lat"] = point["lat"]+float(sys.argv[3])
-        print(point["lat"])
-        print(point["lon"])
-        print()
-    print()
+        point["lat"] = point["lat"]+float(sys.argv[4])
 
+for point in json_file["paddock"]["boundaries"]:
+    point["lat"] = point["lat"]+float(sys.argv[3])
+    point["lat"] = point["lat"]+float(sys.argv[4])
+
+for point in json_file["paddock"]["green_area"]:
+    point["lat"] = point["lat"]+float(sys.argv[3])
+    point["lat"] = point["lat"]+float(sys.argv[4])
+
+
+
+for element in json_file["supplementary_data"]:
+    if element["type"] == "Group":
+        for sub_element in element["children"]:
+            if sub_element["type"] == "Group":
+                for sub_sub_element in sub_element["children"]:
+                    if sub_sub_element["type"] == "Group":
+                        print("Go deeper")
+                    else:
+                        for point in sub_sub_element["points"]:
+                            point["lat"] = point["lat"]+float(sys.argv[3])
+                            point["lat"] = point["lat"]+float(sys.argv[4])
+            else:
+                for point in sub_element["points"]:
+                    point["lat"] = point["lat"]+float(sys.argv[3])
+                    point["lat"] = point["lat"]+float(sys.argv[4])
+    else:
+        for point in element["points"]:
+            point["lat"] = point["lat"]+float(sys.argv[3])
+            point["lat"] = point["lat"]+float(sys.argv[4])
+print()
 
 with open(sys.argv[2], "w") as write_file:
     json.dump(json_file, write_file)
