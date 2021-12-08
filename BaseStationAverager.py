fe32319a1dc845fb4693bb03e93eef2a9027e7e4
Branyon Apel <Branyon.Apel@swarmfarm.com>
2021-12-08 13:18:28 +1000

    Improved with STDdevs

--- BaseStationAverager.py
+++ BaseStationAverager.py
@@ -1,24 +1,22 @@
-import csv, sys
+import csv, sys, statistics
 
 # Use like python3 BaseStationAverager.py IN
 # Takes a document of serial output from a GPS, extracts the GPGGA strings and averages them
 
 reader = csv.reader(open(sys.argv[1]))
 
-total_lat = 0.0
-total_lon = 0.0
-total_alt = 0.0
-points = 0.0
+lats = []
+lons = []
+alts = []
 
 for row in reader:
     if row[0] == "$GPGGA":
-        total_lat = total_lat + float(row[2])
-        total_lon = total_lon + float(row[4])
-        total_alt = total_alt + float(row[9])
-        points = points + 1.0;
+        lats.append(float(row[2]))
+        lons.append(float(row[4]))
+        alts.append(float(row[9]))
 
 # This is just beautiful maths, all because GGA strings encode their data as dddmm.mmmmmm which is just stupid
-print((((total_lat / points) % 100) / 60) + int(total_lat / points / 100))
-print((((total_lon / points) % 100) / 60) + int(total_lon / points / 100))
-print(total_alt / float(points))
-print("Averaged over " + str(points) + " points")
+print(str(((statistics.mean(lats) % 100) / 60) + int(statistics.mean(lats) / 100)) + "\t" + str(statistics.stdev(lats)))
+print(str(((statistics.mean(lons) % 100) / 60) + int(statistics.mean(lons) / 100)) + "\t" + str(statistics.stdev(lons)))
+print(str(statistics.mean(alts)) + "\t" + str(statistics.stdev(alts)))
+print("Averaged over " + str(len(alts)) + " points")
